067998dd0ce266cf34ecaaa098594f9db3fd433e
Michael Gleeson <michael.gleeson@vroom.com>
2020-04-28 14:34:05 -0400

    Variables and References

--- basicsyntax/variables.py
+++ basicsyntax/variables.py
@@ -1,1 +1,23 @@
+
+# string variables
 a = "nyc"
+b = "nyc"
+
+# This print command prints the initial value of a
+print(a)
+
+# int variables (changes value of a from String to Int)
+a = 123
+
+# prints the replacement value of a
+print(a)
+print(b)
+
+c = 'nyc'
+d = c
+
+# Compares the values of c and d
+print(c == d)
+print(d is c)
+
+
