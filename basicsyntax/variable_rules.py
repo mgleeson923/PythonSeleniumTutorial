60b197e261f4642f2c874c29bfbc25140ec3a2fd
Michael Gleeson <michael.gleeson@vroom.com>
2020-04-28 14:51:10 -0400

    Learned some keywords and weird ways that I should't initialize variables

---
+++ basicsyntax/variable_rules.py
@@ -0,0 +1,15 @@
+import keyword
+
+# Prints a list of all the keywords imported
+print(keyword.kwlist)
+
+# initializes multiple variable at once. Works, but frowned upon. Try not to do this
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+
+x, y, z = 10, 30, 30
+print(x)
+print(y)
+print(z)
