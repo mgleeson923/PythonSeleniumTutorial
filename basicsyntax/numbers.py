ae42eb999beca222e247355caf0e4e91c9b4abfe
Michael Gleeson <michael.gleeson@vroom.com>
2020-04-28 16:05:49 -0400

    Math Operators

---
+++ basicsyntax/numbers.py
@@ -0,0 +1,23 @@
+# experimenting with different number types, and math operators
+int_num = 10
+float_num = 20.0
+
+print(int_num)
+print(float_num)
+
+a = 10
+b = 20
+
+add = a + b
+print(add)
+
+diff = b - a
+print(diff)
+
+multi = a * b
+print(multi)
+
+div = b / a
+print(div)
+
+
