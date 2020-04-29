f74d8b766cbf2c043fe80eb802bec9eb948387c1
Michael Gleeson <michael.gleeson@vroom.com>
2020-04-29 15:26:31 -0400

    More Math Syntax

---
+++ basicsyntax/number_operations.py
@@ -0,0 +1,18 @@
+# Number Exponents and Modulo, and Order of Operations
+""" Triple quotes create comments as well """
+
+exponents = 2 ** 3
+
+print(exponents)
+
+""" Modulus """
+solution = 100 % 3
+
+print(solution)
+
+""" Order of Operations """
+# Follows standard Order of Op
+
+result = 2 + 4 * 3 / 2
+
+print(result)
