6599689ae28b5018a5f9350ba78d459559de5c81
Michael Gleeson <michael.gleeson@vroom.com>
2020-04-29 17:03:04 -0400

    Using Strings in Python

---
+++ basicsyntax/strings.py
@@ -0,0 +1,13 @@
+""" Examples of Strings in Python"""
+
+a = "This is a string"
+b = "23 is also a String"
+c = 'Single quotes work too'
+d = "I need to use 'quotes' inside of my string"
+e = "I can also use backlashes to create a \"String\" "
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
