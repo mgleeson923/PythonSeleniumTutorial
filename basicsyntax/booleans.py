39c367dfa17d4e73de82c9d8826623b5810f2d97
Michael Gleeson <michael.gleeson@vroom.com>
2020-04-29 15:35:05 -0400

    Booleans

---
+++ basicsyntax/booleans.py
@@ -0,0 +1,25 @@
+"""Boolean Examples in Python Syntax"""
+
+a = True
+b = False
+
+print(a)
+print(b)
+
+# Bool function of Python. Returns the value of an object
+# 0 is always false
+bool(0)
+bool(1)
+
+print(bool(0))
+print(bool(1))
+
+# An empty string or Null Value is always false
+c = ""
+
+print(bool(c))
+
+c = "some stuff"
+print(bool(c))
+
+
